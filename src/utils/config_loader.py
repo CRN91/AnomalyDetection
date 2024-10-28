5929fb30f779d3e32612acb05e13f6eb1f8565dd
CRN91 <ak824@exeter.ac.uk>
2024-10-28 18:16:50 +0000

    Visualiser now doesn't crash at the end of simulation and instead stops the matplotlib animation. Visualiser can now use config

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -3,6 +3,14 @@ import json
 
 def load_config(file_path='config.json'):
   config_path = os.path.join(os.path.dirname(__file__), file_path)
-  with open(config_path, 'r') as file:
-    config = json.load(file)
+  try:
+    with open(config_path, 'r') as file:
+      config = json.load(file)
+  except FileNotFoundError:
+    # Default config is hard coded
+     config = {
+      "pipeline_name": "Easington-Langeled",
+      "max_capacity": 75,
+      "baseline_file": "Monthly_Baselines.csv"
+    }
   return config
--- src/visualiser/visualiser.py
+++ src/visualiser/visualiser.py
@@ -2,9 +2,16 @@ import matplotlib;matplotlib.use("TkAgg")
 from matplotlib.animation import FuncAnimation
 from src.simulator import run_simulation
 import matplotlib.pyplot as plt
+from src.utils import load_config
+
+config = load_config()
+try:
+  terminal_name = config['pipeline_name']
+except KeyError:
+  terminal_name = 'Easington Langeled'
 
 # Global simulation variables
-SIMULATION_DURATION = 1000
+SIMULATION_DURATION = 10
 MINUTES_PER_DAY = 1440
 UPDATE_INTERVAL = 100  # milliseconds
 
@@ -19,7 +26,7 @@ def setup_plot():
   fig.canvas.manager.set_window_title('Gas Flow Simulation')
 
   # Figure properties
-  ax.set_title("Instantaneous Gas Flow from Easington Langeled")
+  ax.set_title(f"Instantaneous Gas Flow from {terminal_name}")
   ax.set_xlabel("Simulation Duration (Days)")
   ax.set_ylabel("Instantaneous Gas Flow (mcm/day)")
   ax.grid(True, alpha=0.2)
@@ -31,26 +38,31 @@ def setup_plot():
 
 def animate(i, line, ax):
   global x_days, x_vals, y_vals
-  x_vals.extend([x_days + (i/1440) for i in range(1440)])  # Convert minutes to days
-  y_vals.extend(next(simulation))
-  x_days += 1
+  try:
+    x_vals.extend([x_days + (i/1440) for i in range(1440)])  # Convert minutes to days
+    y_vals.extend(next(simulation))
+    x_days += 1
 
-  # Update the line data
-  line.set_data(x_vals, y_vals)
-  # Scroll the x-axis
-  if len(x_vals) > 525960:
-    ax.set_xlim(x_vals[-525960], x_vals[-1])
+    # Update the line data
+    line.set_data(x_vals, y_vals)
+    # Scroll the x-axis
+    if len(x_vals) > 525960:
+      ax.set_xlim(x_vals[-525960], x_vals[-1])
 
-  # Adjust the view limits
-  ax.relim()
-  ax.autoscale_view()
+    # Adjust the view limits
+    ax.relim()
+    ax.autoscale_view()
 
-  plt.tight_layout()
+    plt.tight_layout()
+
+  except RuntimeError:
+    ani.event_source.stop()
 
 def main():
   fig, ax, line = setup_plot()
 
   # Create animation
+  global ani
   ani = FuncAnimation(fig,animate,fargs=(line,ax,),interval=UPDATE_INTERVAL)
 
   plt.show()
